757d2a08974e8334b12d672e5e9707bbddbf2dcb
flaviogp <flavio.gpinheiro@hotmail.com>
2024-11-13 10:25:56 -0300

    feat: add transaction type badge

---
+++ app/transactions/_columns/_components/type-badge.tsx
@@ -0,0 +1,34 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.DEPOSIT) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={10} />
+        Deposito
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="text-danger bg-danger hover:bg-danger bg-opacity-10 font-bold hover:bg-opacity-10">
+        <CircleIcon className="fill-danger mr-2" size={10} />
+        Despesa
+      </Badge>
+    );
+  }
+  return (
+    <Badge className="bg-white bg-opacity-10 font-bold text-white hover:bg-white hover:bg-opacity-10">
+      <CircleIcon className="mr-2 fill-white" size={10} />
+      Investimento
+    </Badge>
+  );
+};
+
+export default TransactionTypeBadge;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { db } from "../_lib/prisma";
+import { Button } from "../_components/ui/button";
+import { ArrowDownUpIcon } from "lucide-react";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
 
-const Transacton = () => {
-  return <div>Transacton</div>;
+const Transacton = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* Title & Button  */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adcionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default Transacton;
