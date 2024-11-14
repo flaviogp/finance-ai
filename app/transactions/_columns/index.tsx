fd5bb1de9e5775e8350193973ff8667dc4277b3a
flaviogp <flavio.gpinheiro@hotmail.com>
2024-11-14 11:15:48 -0300

    fix:  fixed date and value do not appear on the screen

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -7,9 +7,6 @@ import TransactionTypeBadge from "./_components/type-badge";
 import { Button } from "@/app/_components/ui/button";
 import { PencilIcon, TrashIcon } from "lucide-react";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-
 export const TRANSACTION_CATEGORY_LABELS = {
   EDUCATION: "Educação",
   ENTERTAINMENT: "Entretenimento",
@@ -59,23 +56,21 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "date",
     header: "Data",
-    cell: ({ row: { original: transaction } }) => {
+    cell: ({ row: { original: transaction } }) =>
       new Date(transaction.date).toLocaleDateString("pt-BR", {
         day: "2-digit",
         month: "long",
         year: "numeric",
-      });
-    },
+      }),
   },
   {
     accessorKey: "amount",
     header: "Valor",
-    cell: ({ row: { original: transaction } }) => {
+    cell: ({ row: { original: transaction } }) =>
       new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL",
-      }).format(Number(transaction.amount));
-    },
+      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
