43db134f6c936f0e4e0987cdba46122e9763a9b0
flaviogp <flavio.gpinheiro@hotmail.com>
2024-11-20 11:33:19 -0300

    fix: fix database columns name

--- prisma/migrations/20241120131616_fix/migration.sql
+++ prisma/migrations/20241120131616_fix/migration.sql
@@ -3,12 +3,12 @@
 
   - You are about to drop the column `amount` on the `Transaction` table. All the data in the column will be lost.
   - You are about to drop the column `userID` on the `Transaction` table. All the data in the column will be lost.
-  - Added the required column `ammount` to the `Transaction` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `amount` to the `Transaction` table without a default value. This is not possible if the table is not empty.
   - Added the required column `userId` to the `Transaction` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "Transaction" DROP COLUMN "amount",
 DROP COLUMN "userID",
-ADD COLUMN     "ammount" DECIMAL(10,2) NOT NULL,
+ADD COLUMN     "amount" DECIMAL(10,2) NOT NULL,
 ADD COLUMN     "userId" TEXT NOT NULL;
